66b5071db2637516524f4c02019e614887177e85
Miller Berto Simberg Anaia <mler333@hotmail.com>
2015-12-31 19:13:43 -0200

    Version 0.5.3 - Code modularity restored. Thanks Freund.

--- TheckStudios Management Sim API 2015/Main.vb
+++ TheckStudios Management Sim API 2015/Main.vb
@@ -15,151 +15,164 @@ Public Class Resources
     End Sub
 End Class
 Public Class Mechanics
+    Private Shared Sub HardTax()
+        Try
+            'For hard difficulty: enforces 35% of Taxes.
+            'Taxes are automaticly calculated and debted from the player's wealth. Just call them in-game.
+            Dim TaxInfo = Environment.CurrentDirectory + "/Data/TaxInfo.txt"
+            'This is necessary to save on the disk how much money the player has after being charged taxes.
+            Dim PlayerWealthPath = Environment.CurrentDirectory + "/Data/FinanceInfo.txt"
+            'This is necessary to know how much money the player has.
+            Dim PlayerWealth = My.Computer.FileSystem.ReadAllText(Environment.CurrentDirectory + "/Data/FinanceInfo.txt")
+            'TaxTotal is the playerwealth minus 85%, so we can calculate 15% of it to charge taxes.
+            Dim TaxTotal = PlayerWealth - 65%
+            'TaxDebted is the player's wealth - how much taxes will be charged.
+            Dim NewPlayerWealth = PlayerWealth - TaxTotal
+
+            'Write to disk the new playerwealth.
+            Dim WriteToGameInfo As StreamWriter
+            WriteToGameInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
+            WriteToGameInfo.WriteLine(NewPlayerWealth)
+            WriteToGameInfo.Close()
+
+            'This is simply so the game knows how much tax were charged on player, if the developer wishes to disclose it..
+            Dim WriteToGameInfo2 As StreamWriter
+            WriteToGameInfo2 = My.Computer.FileSystem.OpenTextFileWriter(TaxInfo, False)
+            WriteToGameInfo2.WriteLine(TaxTotal)
+            WriteToGameInfo2.Close()
+        Catch HardTaxesException As Exception
+            'If any problems happen while calling taxes (Usually lacking files), state it to the end-user.
+            MsgBox(HardTaxesException.ToString)
+            MsgBox("Failed to call taxes! Files must be missing from the Data folder!")
+        End Try
+    End Sub
+    Private Shared Sub EasyTax()
+        Try
+            'For easy difficulty: enforces 15% of Taxes.
+            'Taxes are automaticly calculated and debted from the player's wealth. Just call them in-game.
+            Dim TaxInfo = Environment.CurrentDirectory + "/Data/TaxInfo.txt"
+            'This is necessary to save on the disk how much money the player has after being charged taxes.
+            Dim PlayerWealthPath = Environment.CurrentDirectory + "/Data/FinanceInfo.txt"
+            'This is necessary to know how much money the player has.
+            Dim PlayerWealth = My.Computer.FileSystem.ReadAllText(Environment.CurrentDirectory + "/Data/FinanceInfo.txt")
+            'TaxTotal is the playerwealth minus 85%, so we can calculate 15% of it to charge taxes.
+            Dim TaxTotal = PlayerWealth - 85%
+            'TaxDebted is the player's wealth - how much taxes will be charged.
+            Dim NewPlayerWealth = PlayerWealth - TaxTotal
+
+            'Write to disk the new playerwealth.
+            Dim WriteToGameInfo As StreamWriter
+            WriteToGameInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
+            WriteToGameInfo.WriteLine(NewPlayerWealth)
+            WriteToGameInfo.Close()
+
+            'This is simply so the game knows how much tax were charged on player, if the developer wishes to disclose it..
+            Dim WriteToGameInfo2 As StreamWriter
+            WriteToGameInfo2 = My.Computer.FileSystem.OpenTextFileWriter(TaxInfo, False)
+            WriteToGameInfo2.WriteLine(TaxTotal)
+            WriteToGameInfo2.Close()
+        Catch EasyTaxesException As Exception
+            'If any problems happen while calling taxes (Usually lacking files), state it to the end-user.
+            MsgBox(EasyTaxesException.ToString)
+            MsgBox("Failed to call taxes! Files must be missing from the Data folder!")
+        End Try
+    End Sub
+
     Public Shared Sub Taxes()
         'Checks what difficulty the player is playing with, and calls the correct taxes.
         If My.Computer.FileSystem.ReadAllText(Environment.CurrentDirectory + "/Data/DifficultyInfo.txt") = 0 Then
-            Try
-                'For easy difficulty: enforces 15% of Taxes.
-                'Taxes are automaticly calculated and debted from the player's wealth. Just call them in-game.
-                Dim TaxInfo = Environment.CurrentDirectory + "/Data/TaxInfo.txt"
-                'This is necessary to save on the disk how much money the player has after being charged taxes.
-                Dim PlayerWealthPath = Environment.CurrentDirectory + "/Data/FinanceInfo.txt"
-                'This is necessary to know how much money the player has.
-                Dim PlayerWealth = My.Computer.FileSystem.ReadAllText(Environment.CurrentDirectory + "/Data/FinanceInfo.txt")
-                'TaxTotal is the playerwealth minus 85%, so we can calculate 15% of it to charge taxes.
-                Dim TaxTotal = PlayerWealth - 85%
-                'TaxDebted is the player's wealth - how much taxes will be charged.
-                Dim NewPlayerWealth = PlayerWealth - TaxTotal
-
-                'Write to disk the new playerwealth.
-                Dim WriteToGameInfo As StreamWriter
-                WriteToGameInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
-                WriteToGameInfo.WriteLine(NewPlayerWealth)
-                WriteToGameInfo.Close()
-
-                'This is simply so the game knows how much tax were charged on player, if the developer wishes to disclose it..
-                Dim WriteToGameInfo2 As StreamWriter
-                WriteToGameInfo2 = My.Computer.FileSystem.OpenTextFileWriter(TaxInfo, False)
-                WriteToGameInfo2.WriteLine(TaxTotal)
-                WriteToGameInfo2.Close()
-            Catch EasyTaxesException As Exception
-                'If any problems happen while calling taxes (Usually lacking files), state it to the end-user.
-                MsgBox(EasyTaxesException.ToString)
-                MsgBox("Failed to call taxes! Files must be missing from the Data folder!")
-            End Try
+            EasyTax()
 
         Else
-            Try
-                'For hard difficulty: enforces 35% of Taxes.
-                'Taxes are automaticly calculated and debted from the player's wealth. Just call them in-game.
-                Dim TaxInfo = Environment.CurrentDirectory + "/Data/TaxInfo.txt"
-                'This is necessary to save on the disk how much money the player has after being charged taxes.
-                Dim PlayerWealthPath = Environment.CurrentDirectory + "/Data/FinanceInfo.txt"
-                'This is necessary to know how much money the player has.
-                Dim PlayerWealth = My.Computer.FileSystem.ReadAllText(Environment.CurrentDirectory + "/Data/FinanceInfo.txt")
-                'TaxTotal is the playerwealth minus 85%, so we can calculate 15% of it to charge taxes.
-                Dim TaxTotal = PlayerWealth - 65%
-                'TaxDebted is the player's wealth - how much taxes will be charged.
-                Dim NewPlayerWealth = PlayerWealth - TaxTotal
-
-                'Write to disk the new playerwealth.
-                Dim WriteToGameInfo As StreamWriter
-                WriteToGameInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
-                WriteToGameInfo.WriteLine(NewPlayerWealth)
-                WriteToGameInfo.Close()
-
-                'This is simply so the game knows how much tax were charged on player, if the developer wishes to disclose it..
-                Dim WriteToGameInfo2 As StreamWriter
-                WriteToGameInfo2 = My.Computer.FileSystem.OpenTextFileWriter(TaxInfo, False)
-                WriteToGameInfo2.WriteLine(TaxTotal)
-                WriteToGameInfo2.Close()
-            Catch HardTaxesException As Exception
-                'If any problems happen while calling taxes (Usually lacking files), state it to the end-user.
-                MsgBox(HardTaxesException.ToString)
-                MsgBox("Failed to call taxes! Files must be missing from the Data folder!")
-            End Try
+            HardTax()
 
         End If
+    End Sub
+    Private Shared Sub EasyInflation()
+        Try
+            'This is necessary to know how much money the player has.
+            Dim PlayerWealth = My.Computer.FileSystem.ReadAllText(Environment.CurrentDirectory + "/Data/FinanceInfo.txt")
+            'This is necessary to save on the disk how much the player has after being charged inflation.
+            Dim PlayerWealthPath = Environment.CurrentDirectory + "/Data/FinanceInfo.txt"
+            'If the player has too much money, inflation will be more punishing
+            If PlayerWealth > "25000" Then
+                Dim Inflation = 0.89
+                Dim InflationDifference = PlayerWealth * Inflation
+                Dim WriteToFinanceInfo As StreamWriter
+                WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
+                WriteToFinanceInfo.WriteLine(InflationDifference)
+                WriteToFinanceInfo.Close()
+            End If
+            'If the player has a bit of money, inflation will be less punishing.
+            If PlayerWealth < "5000" And PlayerWealth > "2500" Then
+                Dim Inflation = 0.93
+                Dim InflationDifference = PlayerWealth * Inflation
+                Dim WriteToFinanceInfo As StreamWriter
+                WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
+                WriteToFinanceInfo.WriteLine(InflationDifference)
+                WriteToFinanceInfo.Close()
+            End If
+            'If the player has a bit of money, inflation will be less punishing.
+            If PlayerWealth < "2500" Then
+                Dim Inflation = 0.98
+                Dim InflationDifference = PlayerWealth * Inflation
+                Dim WriteToFinanceInfo As StreamWriter
+                WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
+                WriteToFinanceInfo.WriteLine(InflationDifference)
+                WriteToFinanceInfo.Close()
+            End If
+        Catch EasyInflationException As Exception
+            'If any problems happen while calling Inflation (Usually lacking files), state it to the end-user.
+            MsgBox(EasyInflationException.ToString)
+            MsgBox("Failed to call Inflation! Files must be missing from the Data folder!")
+        End Try
+    End Sub
+    Private Shared Sub HardInflation()
+        Try
+            'This is necessary to know how much money the player has.
+            Dim PlayerWealth = My.Computer.FileSystem.ReadAllText(Environment.CurrentDirectory + "/Data/FinanceInfo.txt")
+            'This is necessary to save on the disk how much the player has after being charged inflation.
+            Dim PlayerWealthPath = Environment.CurrentDirectory + "/Data/FinanceInfo.txt"
+            'If the player has too much money, inflation will be more punishing
+            If PlayerWealth > "25000" Then
+                Dim Inflation = 0.75
+                Dim InflationDifference = PlayerWealth * Inflation
+                Dim WriteToFinanceInfo As StreamWriter
+                WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
+                WriteToFinanceInfo.WriteLine(InflationDifference)
+                WriteToFinanceInfo.Close()
+            End If
+            'If the player has a bit of money, inflation will be less punishing.
+            If PlayerWealth < "5000" And PlayerWealth > "2500" Then
+                Dim Inflation = 0.8
+                Dim InflationDifference = PlayerWealth * Inflation
+                Dim WriteToFinanceInfo As StreamWriter
+                WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
+                WriteToFinanceInfo.WriteLine(InflationDifference)
+                WriteToFinanceInfo.Close()
+            End If
+            'If the player has a bit of money, inflation will be less punishing.
+            If PlayerWealth < "2500" Then
+                Dim Inflation = 0.85
+                Dim InflationDifference = PlayerWealth * Inflation
+                Dim WriteToFinanceInfo As StreamWriter
+                WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
+                WriteToFinanceInfo.WriteLine(InflationDifference)
+                WriteToFinanceInfo.Close()
+            End If
+        Catch HardInflationException As Exception
+            'If any problems happen while calling Inflation (Usually lacking files), state it to the end-user.
+            MsgBox(HardInflationException.ToString)
+            MsgBox("Failed to call Inflation! Files must be missing from the Data folder!")
+        End Try
     End Sub
     Public Shared Sub Inflation()
         'Checks what difficulty the player is playing with, and calls the correct inflation.
         If My.Computer.FileSystem.ReadAllText(Environment.CurrentDirectory + "/Data/DifficultyInfo.txt") = 0 Then
-            Try
-                'This is necessary to know how much money the player has.
-                Dim PlayerWealth = My.Computer.FileSystem.ReadAllText(Environment.CurrentDirectory + "/Data/FinanceInfo.txt")
-                'This is necessary to save on the disk how much the player has after being charged inflation.
-                Dim PlayerWealthPath = Environment.CurrentDirectory + "/Data/FinanceInfo.txt"
-                'If the player has too much money, inflation will be more punishing
-                If PlayerWealth > "25000" Then
-                    Dim Inflation = 0.89
-                    Dim InflationDifference = PlayerWealth * Inflation
-                    Dim WriteToFinanceInfo As StreamWriter
-                    WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
-                    WriteToFinanceInfo.WriteLine(InflationDifference)
-                    WriteToFinanceInfo.Close()
-                End If
-                'If the player has a bit of money, inflation will be less punishing.
-                If PlayerWealth < "5000" And PlayerWealth > "2500" Then
-                    Dim Inflation = 0.93
-                    Dim InflationDifference = PlayerWealth * Inflation
-                    Dim WriteToFinanceInfo As StreamWriter
-                    WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
-                    WriteToFinanceInfo.WriteLine(InflationDifference)
-                    WriteToFinanceInfo.Close()
-                End If
-                'If the player has a bit of money, inflation will be less punishing.
-                If PlayerWealth < "2500" Then
-                    Dim Inflation = 0.98
-                    Dim InflationDifference = PlayerWealth * Inflation
-                    Dim WriteToFinanceInfo As StreamWriter
-                    WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
-                    WriteToFinanceInfo.WriteLine(InflationDifference)
-                    WriteToFinanceInfo.Close()
-                End If
-            Catch EasyInflationException As Exception
-                'If any problems happen while calling Inflation (Usually lacking files), state it to the end-user.
-                MsgBox(EasyInflationException.ToString)
-                MsgBox("Failed to call Inflation! Files must be missing from the Data folder!")
-            End Try
+            EasyInflation()
 
         Else
-            Try
-                'This is necessary to know how much money the player has.
-                Dim PlayerWealth = My.Computer.FileSystem.ReadAllText(Environment.CurrentDirectory + "/Data/FinanceInfo.txt")
-                'This is necessary to save on the disk how much the player has after being charged inflation.
-                Dim PlayerWealthPath = Environment.CurrentDirectory + "/Data/FinanceInfo.txt"
-                'If the player has too much money, inflation will be more punishing
-                If PlayerWealth > "25000" Then
-                    Dim Inflation = 0.75
-                    Dim InflationDifference = PlayerWealth * Inflation
-                    Dim WriteToFinanceInfo As StreamWriter
-                    WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
-                    WriteToFinanceInfo.WriteLine(InflationDifference)
-                    WriteToFinanceInfo.Close()
-                End If
-                'If the player has a bit of money, inflation will be less punishing.
-                If PlayerWealth < "5000" And PlayerWealth > "2500" Then
-                    Dim Inflation = 0.8
-                    Dim InflationDifference = PlayerWealth * Inflation
-                    Dim WriteToFinanceInfo As StreamWriter
-                    WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
-                    WriteToFinanceInfo.WriteLine(InflationDifference)
-                    WriteToFinanceInfo.Close()
-                End If
-                'If the player has a bit of money, inflation will be less punishing.
-                If PlayerWealth < "2500" Then
-                    Dim Inflation = 0.85
-                    Dim InflationDifference = PlayerWealth * Inflation
-                    Dim WriteToFinanceInfo As StreamWriter
-                    WriteToFinanceInfo = My.Computer.FileSystem.OpenTextFileWriter(PlayerWealthPath, False)
-                    WriteToFinanceInfo.WriteLine(InflationDifference)
-                    WriteToFinanceInfo.Close()
-                End If
-            Catch HardInflationException As Exception
-                'If any problems happen while calling Inflation (Usually lacking files), state it to the end-user.
-                MsgBox(HardInflationException.ToString)
-                MsgBox("Failed to call Inflation! Files must be missing from the Data folder!")
-            End Try
+            HardInflation()
         End If
     End Sub
 
