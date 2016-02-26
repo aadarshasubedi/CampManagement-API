6219bbdfd99e890ae44827856cfac74e95839878
Miller Berto Simberg Anaia <mler333@hotmail.com>
2016-02-26 20:36:00 -0300

    Small improvements. Playable.

--- CampManagement/CampManagementPolicies.Designer.vb
+++ CampManagement/CampManagementPolicies.Designer.vb
@@ -22,9 +22,7 @@ Partial Class CampManagementPolicies
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.MarketPanel = New System.Windows.Forms.Panel()
-        Me.MarketLabel = New System.Windows.Forms.Label()
-        Me.NewGameButton = New System.Windows.Forms.Button()
+        Me.BackToGameButton = New System.Windows.Forms.Button()
         Me.PoliciesPanel = New System.Windows.Forms.Panel()
         Me.RequestPrisonersInfo = New System.Windows.Forms.Label()
         Me.KillPrisonersInfo = New System.Windows.Forms.Label()
@@ -32,48 +30,38 @@ Partial Class CampManagementPolicies
         Me.EatAllYouWantInfo = New System.Windows.Forms.Label()
         Me.TenHourInfo = New System.Windows.Forms.Label()
         Me.WorkToDeathInfo = New System.Windows.Forms.Label()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.RequestPrisonersCheckBox = New System.Windows.Forms.CheckBox()
+        Me.ExecutePrisonersCheckBox = New System.Windows.Forms.CheckBox()
         Me.TwoMealsPolicyCheckBox = New System.Windows.Forms.CheckBox()
         Me.EatAllYouCanPolicyCheckBox = New System.Windows.Forms.CheckBox()
         Me.EasierWorkPolicyCheckBox = New System.Windows.Forms.CheckBox()
         Me.ExhaustionPolicyCheckBox = New System.Windows.Forms.CheckBox()
         Me.PolicyLabel = New System.Windows.Forms.Label()
-        Me.MarketPanel.SuspendLayout()
+        Me.BuyWoodLabel = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.BuyFoodLabel = New System.Windows.Forms.Label()
+        Me.BuyWaterLabel = New System.Windows.Forms.Label()
+        Me.FoodLabel = New System.Windows.Forms.Label()
+        Me.WaterLabel = New System.Windows.Forms.Label()
+        Me.WoodLabel = New System.Windows.Forms.Label()
+        Me.MarketPanelLabel = New System.Windows.Forms.Label()
+        Me.SellFoodLabel = New System.Windows.Forms.Label()
+        Me.SellWaterLabel = New System.Windows.Forms.Label()
+        Me.SellWoodLabel = New System.Windows.Forms.Label()
         Me.PoliciesPanel.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'MarketPanel
+        'BackToGameButton
         '
-        Me.MarketPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.MarketPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.MarketPanel.Controls.Add(Me.MarketLabel)
-        Me.MarketPanel.Location = New System.Drawing.Point(12, 12)
-        Me.MarketPanel.Name = "MarketPanel"
-        Me.MarketPanel.Size = New System.Drawing.Size(212, 252)
-        Me.MarketPanel.TabIndex = 1
-        '
-        'MarketLabel
-        '
-        Me.MarketLabel.AutoSize = True
-        Me.MarketLabel.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MarketLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.MarketLabel.Location = New System.Drawing.Point(64, 9)
-        Me.MarketLabel.Name = "MarketLabel"
-        Me.MarketLabel.Size = New System.Drawing.Size(58, 17)
-        Me.MarketLabel.TabIndex = 15
-        Me.MarketLabel.Text = "MARKET" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
-        '
-        'NewGameButton
-        '
-        Me.NewGameButton.BackColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.NewGameButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.NewGameButton.Location = New System.Drawing.Point(171, 324)
-        Me.NewGameButton.Name = "NewGameButton"
-        Me.NewGameButton.Size = New System.Drawing.Size(113, 24)
-        Me.NewGameButton.TabIndex = 1
-        Me.NewGameButton.Text = "Go back."
-        Me.NewGameButton.UseVisualStyleBackColor = False
+        Me.BackToGameButton.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.BackToGameButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BackToGameButton.Location = New System.Drawing.Point(168, 418)
+        Me.BackToGameButton.Name = "BackToGameButton"
+        Me.BackToGameButton.Size = New System.Drawing.Size(113, 24)
+        Me.BackToGameButton.TabIndex = 1
+        Me.BackToGameButton.Text = "Go back."
+        Me.BackToGameButton.UseVisualStyleBackColor = False
         '
         'PoliciesPanel
         '
@@ -85,16 +73,16 @@ Partial Class CampManagementPolicies
         Me.PoliciesPanel.Controls.Add(Me.EatAllYouWantInfo)
         Me.PoliciesPanel.Controls.Add(Me.TenHourInfo)
         Me.PoliciesPanel.Controls.Add(Me.WorkToDeathInfo)
-        Me.PoliciesPanel.Controls.Add(Me.CheckBox1)
-        Me.PoliciesPanel.Controls.Add(Me.CheckBox2)
+        Me.PoliciesPanel.Controls.Add(Me.RequestPrisonersCheckBox)
+        Me.PoliciesPanel.Controls.Add(Me.ExecutePrisonersCheckBox)
         Me.PoliciesPanel.Controls.Add(Me.TwoMealsPolicyCheckBox)
         Me.PoliciesPanel.Controls.Add(Me.EatAllYouCanPolicyCheckBox)
         Me.PoliciesPanel.Controls.Add(Me.EasierWorkPolicyCheckBox)
         Me.PoliciesPanel.Controls.Add(Me.ExhaustionPolicyCheckBox)
         Me.PoliciesPanel.Controls.Add(Me.PolicyLabel)
-        Me.PoliciesPanel.Location = New System.Drawing.Point(230, 12)
+        Me.PoliciesPanel.Location = New System.Drawing.Point(12, 12)
         Me.PoliciesPanel.Name = "PoliciesPanel"
-        Me.PoliciesPanel.Size = New System.Drawing.Size(205, 252)
+        Me.PoliciesPanel.Size = New System.Drawing.Size(423, 252)
         Me.PoliciesPanel.TabIndex = 2
         '
         'RequestPrisonersInfo
@@ -163,27 +151,27 @@ Partial Class CampManagementPolicies
         Me.WorkToDeathInfo.TabIndex = 23
         Me.WorkToDeathInfo.Text = "[?]"
         '
-        'CheckBox1
+        'RequestPrisonersCheckBox
         '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.CheckBox1.Location = New System.Drawing.Point(27, 187)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(159, 17)
-        Me.CheckBox1.TabIndex = 22
-        Me.CheckBox1.Text = "Request 5 prisoners [misc]"
-        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.RequestPrisonersCheckBox.AutoSize = True
+        Me.RequestPrisonersCheckBox.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.RequestPrisonersCheckBox.Location = New System.Drawing.Point(27, 187)
+        Me.RequestPrisonersCheckBox.Name = "RequestPrisonersCheckBox"
+        Me.RequestPrisonersCheckBox.Size = New System.Drawing.Size(159, 17)
+        Me.RequestPrisonersCheckBox.TabIndex = 22
+        Me.RequestPrisonersCheckBox.Text = "Request 5 prisoners [misc]"
+        Me.RequestPrisonersCheckBox.UseVisualStyleBackColor = True
         '
-        'CheckBox2
+        'ExecutePrisonersCheckBox
         '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.CheckBox2.Location = New System.Drawing.Point(27, 164)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(156, 17)
-        Me.CheckBox2.TabIndex = 21
-        Me.CheckBox2.Text = "Execute 5 prisoners [misc]"
-        Me.CheckBox2.UseVisualStyleBackColor = True
+        Me.ExecutePrisonersCheckBox.AutoSize = True
+        Me.ExecutePrisonersCheckBox.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ExecutePrisonersCheckBox.Location = New System.Drawing.Point(27, 164)
+        Me.ExecutePrisonersCheckBox.Name = "ExecutePrisonersCheckBox"
+        Me.ExecutePrisonersCheckBox.Size = New System.Drawing.Size(156, 17)
+        Me.ExecutePrisonersCheckBox.TabIndex = 21
+        Me.ExecutePrisonersCheckBox.Text = "Execute 5 prisoners [misc]"
+        Me.ExecutePrisonersCheckBox.UseVisualStyleBackColor = True
         '
         'TwoMealsPolicyCheckBox
         '
@@ -234,21 +222,150 @@ Partial Class CampManagementPolicies
         Me.PolicyLabel.AutoSize = True
         Me.PolicyLabel.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.PolicyLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.PolicyLabel.Location = New System.Drawing.Point(71, 9)
+        Me.PolicyLabel.Location = New System.Drawing.Point(180, 9)
         Me.PolicyLabel.Name = "PolicyLabel"
         Me.PolicyLabel.Size = New System.Drawing.Size(59, 17)
         Me.PolicyLabel.TabIndex = 16
         Me.PolicyLabel.Text = "POLICIES"
+        '
+        'BuyWoodLabel
+        '
+        Me.BuyWoodLabel.AutoSize = True
+        Me.BuyWoodLabel.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BuyWoodLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BuyWoodLabel.Location = New System.Drawing.Point(51, 27)
+        Me.BuyWoodLabel.Name = "BuyWoodLabel"
+        Me.BuyWoodLabel.Size = New System.Drawing.Size(91, 13)
+        Me.BuyWoodLabel.TabIndex = 17
+        Me.BuyWoodLabel.Text = "BUY  - 10 for $20"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.SellFoodLabel)
+        Me.Panel1.Controls.Add(Me.SellWaterLabel)
+        Me.Panel1.Controls.Add(Me.SellWoodLabel)
+        Me.Panel1.Controls.Add(Me.BuyFoodLabel)
+        Me.Panel1.Controls.Add(Me.BuyWaterLabel)
+        Me.Panel1.Controls.Add(Me.BuyWoodLabel)
+        Me.Panel1.Controls.Add(Me.FoodLabel)
+        Me.Panel1.Controls.Add(Me.WaterLabel)
+        Me.Panel1.Controls.Add(Me.WoodLabel)
+        Me.Panel1.Controls.Add(Me.MarketPanelLabel)
+        Me.Panel1.Location = New System.Drawing.Point(12, 270)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(423, 96)
+        Me.Panel1.TabIndex = 18
+        '
+        'BuyFoodLabel
+        '
+        Me.BuyFoodLabel.AutoSize = True
+        Me.BuyFoodLabel.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BuyFoodLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BuyFoodLabel.Location = New System.Drawing.Point(51, 60)
+        Me.BuyFoodLabel.Name = "BuyFoodLabel"
+        Me.BuyFoodLabel.Size = New System.Drawing.Size(85, 13)
+        Me.BuyFoodLabel.TabIndex = 21
+        Me.BuyFoodLabel.Text = "BUY  - 10 for $5"
+        '
+        'BuyWaterLabel
+        '
+        Me.BuyWaterLabel.AutoSize = True
+        Me.BuyWaterLabel.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BuyWaterLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BuyWaterLabel.Location = New System.Drawing.Point(51, 44)
+        Me.BuyWaterLabel.Name = "BuyWaterLabel"
+        Me.BuyWaterLabel.Size = New System.Drawing.Size(91, 13)
+        Me.BuyWaterLabel.TabIndex = 20
+        Me.BuyWaterLabel.Text = "BUY  - 10 for $10"
+        '
+        'FoodLabel
+        '
+        Me.FoodLabel.AutoSize = True
+        Me.FoodLabel.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FoodLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.FoodLabel.Location = New System.Drawing.Point(3, 59)
+        Me.FoodLabel.Name = "FoodLabel"
+        Me.FoodLabel.Size = New System.Drawing.Size(37, 15)
+        Me.FoodLabel.TabIndex = 19
+        Me.FoodLabel.Text = "Food:"
+        '
+        'WaterLabel
+        '
+        Me.WaterLabel.AutoSize = True
+        Me.WaterLabel.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.WaterLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.WaterLabel.Location = New System.Drawing.Point(3, 43)
+        Me.WaterLabel.Name = "WaterLabel"
+        Me.WaterLabel.Size = New System.Drawing.Size(41, 15)
+        Me.WaterLabel.TabIndex = 18
+        Me.WaterLabel.Text = "Water:"
+        '
+        'WoodLabel
+        '
+        Me.WoodLabel.AutoSize = True
+        Me.WoodLabel.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.WoodLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.WoodLabel.Location = New System.Drawing.Point(3, 26)
+        Me.WoodLabel.Name = "WoodLabel"
+        Me.WoodLabel.Size = New System.Drawing.Size(42, 15)
+        Me.WoodLabel.TabIndex = 17
+        Me.WoodLabel.Text = "Wood:"
+        '
+        'MarketPanelLabel
+        '
+        Me.MarketPanelLabel.AutoSize = True
+        Me.MarketPanelLabel.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MarketPanelLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.MarketPanelLabel.Location = New System.Drawing.Point(3, 5)
+        Me.MarketPanelLabel.Name = "MarketPanelLabel"
+        Me.MarketPanelLabel.Size = New System.Drawing.Size(58, 17)
+        Me.MarketPanelLabel.TabIndex = 15
+        Me.MarketPanelLabel.Text = "MARKET" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'SellFoodLabel
+        '
+        Me.SellFoodLabel.AutoSize = True
+        Me.SellFoodLabel.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SellFoodLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.SellFoodLabel.Location = New System.Drawing.Point(152, 61)
+        Me.SellFoodLabel.Name = "SellFoodLabel"
+        Me.SellFoodLabel.Size = New System.Drawing.Size(87, 13)
+        Me.SellFoodLabel.TabIndex = 24
+        Me.SellFoodLabel.Text = "SELL  - 10 for $3"
+        '
+        'SellWaterLabel
+        '
+        Me.SellWaterLabel.AutoSize = True
+        Me.SellWaterLabel.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SellWaterLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.SellWaterLabel.Location = New System.Drawing.Point(152, 45)
+        Me.SellWaterLabel.Name = "SellWaterLabel"
+        Me.SellWaterLabel.Size = New System.Drawing.Size(87, 13)
+        Me.SellWaterLabel.TabIndex = 23
+        Me.SellWaterLabel.Text = "SELL  - 10 for $5"
+        '
+        'SellWoodLabel
+        '
+        Me.SellWoodLabel.AutoSize = True
+        Me.SellWoodLabel.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SellWoodLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.SellWoodLabel.Location = New System.Drawing.Point(152, 28)
+        Me.SellWoodLabel.Name = "SellWoodLabel"
+        Me.SellWoodLabel.Size = New System.Drawing.Size(93, 13)
+        Me.SellWoodLabel.TabIndex = 22
+        Me.SellWoodLabel.Text = "SELL  - 10 for $10"
         '
         'CampManagementPolicies
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.ClientSize = New System.Drawing.Size(447, 360)
+        Me.ClientSize = New System.Drawing.Size(447, 454)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.PoliciesPanel)
-        Me.Controls.Add(Me.MarketPanel)
-        Me.Controls.Add(Me.NewGameButton)
+        Me.Controls.Add(Me.BackToGameButton)
         Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.MaximizeBox = False
@@ -257,29 +374,37 @@ Partial Class CampManagementPolicies
         Me.ShowIcon = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "CampManagementPolicies"
-        Me.MarketPanel.ResumeLayout(False)
-        Me.MarketPanel.PerformLayout()
         Me.PoliciesPanel.ResumeLayout(False)
         Me.PoliciesPanel.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents MarketPanel As Panel
-    Friend WithEvents NewGameButton As Button
+    Friend WithEvents BackToGameButton As Button
     Friend WithEvents PoliciesPanel As Panel
-    Friend WithEvents MarketLabel As Label
     Friend WithEvents PolicyLabel As Label
     Friend WithEvents ExhaustionPolicyCheckBox As CheckBox
     Friend WithEvents EasierWorkPolicyCheckBox As CheckBox
     Friend WithEvents TwoMealsPolicyCheckBox As CheckBox
     Friend WithEvents EatAllYouCanPolicyCheckBox As CheckBox
-    Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents RequestPrisonersCheckBox As CheckBox
+    Friend WithEvents ExecutePrisonersCheckBox As CheckBox
     Friend WithEvents RequestPrisonersInfo As Label
     Friend WithEvents KillPrisonersInfo As Label
     Friend WithEvents FourMealsADayInfo As Label
     Friend WithEvents EatAllYouWantInfo As Label
     Friend WithEvents TenHourInfo As Label
     Friend WithEvents WorkToDeathInfo As Label
+    Friend WithEvents BuyWoodLabel As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents FoodLabel As Label
+    Friend WithEvents WaterLabel As Label
+    Friend WithEvents WoodLabel As Label
+    Friend WithEvents MarketPanelLabel As Label
+    Friend WithEvents BuyFoodLabel As Label
+    Friend WithEvents BuyWaterLabel As Label
+    Friend WithEvents SellFoodLabel As Label
+    Friend WithEvents SellWaterLabel As Label
+    Friend WithEvents SellWoodLabel As Label
 End Class
--- CampManagement/CampManagementPolicies.vb
+++ CampManagement/CampManagementPolicies.vb
@@ -1,5 +1,49 @@
 ﻿Public Class CampManagementPolicies
-    Private Sub NewGameButton_Click(sender As Object, e As EventArgs) Handles NewGameButton.Click
+    Private Sub BackToGameButton_Click(sender As Object, e As EventArgs) Handles BackToGameButton.Click
         CampManagementMain.Show()
+    End Sub
+    Private Sub ExhaustionPolicyCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles ExhaustionPolicyCheckBox.CheckedChanged
+        EasierWorkPolicyCheckBox.Checked = False
+    End Sub
+
+    Private Sub EasierWorkPolicyCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles EasierWorkPolicyCheckBox.CheckedChanged
+        ExhaustionPolicyCheckBox.Checked = False
+    End Sub
+
+    Private Sub EatAllYouCanPolicyCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles EatAllYouCanPolicyCheckBox.CheckedChanged
+        TwoMealsPolicyCheckBox.Checked = False
+    End Sub
+
+    Private Sub TwoMealsPolicyCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles TwoMealsPolicyCheckBox.CheckedChanged
+        EatAllYouCanPolicyCheckBox.Checked = False
+    End Sub
+    Private Sub ExecutePrisonersCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles ExecutePrisonersCheckBox.CheckedChanged
+        RequestPrisonersCheckBox.Checked = False
+    End Sub
+    Private Sub RequestPrisonersCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles RequestPrisonersCheckBox.CheckedChanged
+        ExecutePrisonersCheckBox.Checked = False
+    End Sub
+    Private Sub WorkToDeathInfo_Click(sender As Object, e As EventArgs) Handles WorkToDeathInfo.Click
+        MsgBox("All profits from workforce receive a 2x multiplier, but 6 prisoners die per week.")
+    End Sub
+
+    Private Sub TenHourInfo_Click(sender As Object, e As EventArgs) Handles TenHourInfo.Click
+        MsgBox("All profits from workforce receive a 1.33x multiplier, but 2 prisoners die per week")
+    End Sub
+
+    Private Sub EatAllYouWantInfo_Click(sender As Object, e As EventArgs) Handles EatAllYouWantInfo.Click
+        MsgBox("All profits from workforce receive 1.33x multiplier, but you lose 5 rations per prisoner per week.")
+    End Sub
+
+    Private Sub FourMealsADayInfo_Click(sender As Object, e As EventArgs) Handles FourMealsADayInfo.Click
+        MsgBox("All profits from workforce receive a 1.22x multiplier, but you lose 2 rations per prisoner per week")
+    End Sub
+
+    Private Sub KillPrisonersInfo_Click(sender As Object, e As EventArgs) Handles KillPrisonersInfo.Click
+        MsgBox("Execute 5 prisoners, and receive a small boost to your finances (20 units).")
+    End Sub
+
+    Private Sub RequestPrisonersInfo_Click(sender As Object, e As EventArgs) Handles RequestPrisonersInfo.Click
+        MsgBox("Request 5 prisoners, and pay a small sum from your finances (20 units).")
     End Sub
 End Class
